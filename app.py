512fdb756d9611669a8bb9d1dc884eb9112cc639
Dmytro Chobotar <dchobotar.fl@gmail.com>
2025-04-21 19:15:54 -0400

    fixed the function names for error handling

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ def create_app():
         return jsonify({"error": "Bad Request"}), 400
     
     @app.errorhandler(404)
-    def bad_request(error):
+    def not_found(error):
         return jsonify({"error": "Not Found"}), 404
     
     @app.errorhandler(500)
-    def bad_request(error):
+    def internal_error(error):
         return jsonify({"error": "Internal Server Error"}), 500
     
     return app
