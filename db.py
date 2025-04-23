3caa5bd8ae71911b3d1d7c6732c33007477dd8a2
Dmytro Chobotar <82722335+dmytro-ch21@users.noreply.github.com>
2025-04-23 19:12:20 -0400

    Deleted unneeded lines of code db.py

--- db.py
+++ db.py
@@ -1,21 +1,3 @@
-from models.product_model import Product
-product1 = Product(
-    sku="P_001",
-    name="iPhone 16 Pro",
-    price=999.99,
-    quantity=100,
-    description="Nice smartphone"
-)
-product2 = Product(
-    sku="P_002",
-    name="Dell Laptop XP500",
-    price=1299.99,
-    quantity=10,
-    description="A high-performance laptop - Intel i7 - Nvidia GTX 5080 - 32GB"
-)
-product_data = [product1, product2]
-
-
 import psycopg2
 from flask import g, current_app
 
